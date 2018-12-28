d162404606a6c78c790d277182d9649065b23068
mattmccj <mattmccj@outlook.com>
2018-12-27 17:26:08 -0800

    added ds_load_random and query_from_file plus other small changes

--- python/src/Client.py
+++ python/src/Client.py
@@ -1,3 +1,41 @@
+#
+# Copyright (c) 2015-2018 in2H2 inc.
+# System developed for in2H2 inc. by Intermotion Technology, Inc.
+#
+# Full system RTL, C sources and board design files available at https://github.com/nearist
+#
+# in2H2 inc. Team Members:
+# - Chris McCormick - Algorithm Research and Design
+# - Matt McCormick - Board Production, System Q/A
+#
+# Intermotion Technology Inc. Team Members:
+# - Mick Fandrich - Project Lead
+# - Dr. Ludovico Minati - Board Architecture and Design, FPGA Technology Advisor
+# - Vardan Movsisyan - RTL Team Lead
+# - Khachatur Gyozalyan - RTL Design
+# - Tigran Papazyan - RTL Design
+# - Taron Harutyunyan - RTL Design
+# - Hayk Ghaltaghchyan - System Software
+#
+# Tecno77 S.r.l. Team Members:
+# - Stefano Aldrigo, Board Layout Design
+#
+# We dedicate this project to the memory of Bruce McCormick, an AI pioneer
+# and advocate, a good friend and father.
+#
+# These materials are provided free of charge: you can redistribute them and/or modify
+# them under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# These materials are distributed in the hope that they will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
 import json
 
 from Common import *
@@ -67,13 +105,13 @@ class Client:
                         if length > 0:
                             body = struct.unpack_from(("=" + ("Q" * length)), r.body, 0)
 
-                            if request.attribute_1 == 0:
+                            if r.attribute_1 == 0:
                                 for i in range(0, length, 2):
                                     if body[i] != 0xFFFFFFFFFFFFFFFF and body[i + 1] != 0xFFFFFFFFFFFFFFFF:
                                         results.append({'ds_id': body[i], 'distance': body[i + 1]})
                                     else:
                                         break
-                            elif request.attribute_1 == 1:
+                            elif r.attribute_1 == 1:
                                 result = []
                                 for i in range(0, length, 2):
                                     if body[i] != 0xFFFFFFFFFFFFFFFF and body[i + 1] != 0xFFFFFFFFFFFFFFFF:
@@ -99,6 +137,18 @@ class Client:
         if status is None:
             return self.__on_request_complete(request)
 
+    def set_keepalive(self, after_idle_sec=7200, interval_sec=75, max_fails=8):
+        if sys.platform == 'linux':
+            self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+            self.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, after_idle_sec)
+            self.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, interval_sec)
+            self.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, max_fails)
+        elif sys.platform == 'darwin':
+            self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+            self.sock.setsockopt(socket.IPPROTO_TCP, 0x10, interval_sec)
+        elif sys.platform == 'win32':
+            self.sock.ioctl(socket.SIO_KEEPALIVE_VALS, (1, after_idle_sec * 1000, interval_sec * 1000))
+
     def open(self, host, port, api_key):
         """
         Open a socket for communication with the Nearist appliance.
@@ -121,6 +171,8 @@ class Client:
 
         # Create a new socket (the host and port are specified in 'connect').
         self.sock = socket.socket(address_info[0][0], socket.SOCK_STREAM)
+
+        self.set_keepalive()
 
         # Connect to the host.
         self.sock.connect((host, port))
@@ -298,10 +350,33 @@ class Client:
         )
         self.__request(request)
 
+    def ds_load_random(self, vector_count, comp_count):
+        """
+        Load random dataset to Nearist appliance
+
+        :type vector_count: integer
+        :param vector_count: Vector count to generate
+
+        :type comp_count: integer
+        :param comp_count: Vector's component count
+
+        """
+
+        root = json.dumps({"vectorCount": vector_count, "compCount": comp_count})
+        request = Request(
+            self.api_key,
+            Command.DS_LOAD,
+            attribute_0=0,
+            attribute_1=2,
+            body_length=len(root),
+            body=root
+        )
+        self.__request(request)
+
     def query(self, vectors, batch_size=128, verbose=True):
         """
         Query for single/multiple vector(s)
-        
+
         :type vectors: list or list of lists
         :param vectors: List of components for single query / List of vectors (component lists) for multipel query
 
@@ -318,24 +393,25 @@ class Client:
                 self.api_key,
                 Command.QUERY,
                 attribute_0=len(vectors),       # Length of a vector
+                attribute_1=0,
                 body_length=len(vectors),       # Total payload size
                 body=vectors
             )
-            
+
             # Issue the query and return the results.
             return self.__request(request)
-            
-        # ======== Batch Query ========    
+
+        # ======== Batch Query ========
         # Otherwise, this is a batch query.
         # We will break the queries into smaller batches and aggregate
         # the results.
-        
+
         start = 0
         results = []
-        
+
         # Record the start time.
         t0 = time.time()
-        
+
         # For each mini-batch...
         while start < len(vectors):
             # Calculate the 'end' of this mini-batch.
@@ -348,11 +424,11 @@ class Client:
             if verbose and not start == 0:
                 # Caclulate the average throughput so far.
                 queries_per_sec = ((time.time() - t0)  / start)
-                
-                # Estimate how much time (in minutes) is left to complete the 
+
+                # Estimate how much time (in minutes) is left to complete the
                 # test.
                 time_est = queries_per_sec * (len(vectors) - start) / 60.0
-                
+
                 # Format the estimated time remaining into minutes.
                 # If it's less than 1 minute, show <1 instead of 0.
                 if time_est < 1:
@@ -360,30 +436,50 @@ class Client:
                 else:
                     time_est_str = '~%.0f min...' % time_est
 
-                print '  Query %5d / %5d (%3.0f%%) Time Remaining: %s' % (start, len(vectors), float(start) / len(vectors) * 100.0, time_est_str)    
+                print '  Query %5d / %5d (%3.0f%%) Time Remaining: %s' % (start, len(vectors), float(start) / len(vectors) * 100.0, time_est_str)
                 sys.stdout.flush()
-            
+
             # Construct the query request.
             request = Request(
                 self.api_key,
                 Command.QUERY,
                 attribute_0=len(mini_batch[0]),    # Length of a vector
-                attribute_1=1,                     # TODO - Currently unused?
+                attribute_1=1,
                 body_length=len(mini_batch) * (len(mini_batch[0])), # Total matrix size
-                body=mini_batch 
+                body=mini_batch
             )
-        
+
             # Submit the query and wait for the results.
             mini_res = self.__request(request)
 
             if not len(mini_batch) == len(mini_res):
                 print 'ERROR: Mini batch length %d does not match results legnth %d!' % (len(mini_batch), len(mini_res))
                 sys.stdout.flush()
-            
+
             # Accumulate the results.
             results = results + mini_res
-            
+
             # Update the start pointer.
             start = end
-            
+
         return results
+
+    def query_from_file(self, file_name, dataset_name, output_name):
+        """
+        Query local dataset to Nearist appliance
+        :param file_name: Local dataset file name
+        :param dataset_name: Local dataset name
+        :param output_name: Results output file name
+        :return:
+        """
+
+        root = json.dumps({"fileName": file_name, "datasetName": dataset_name, "output": output_name})
+        request = Request(
+            self.api_key,
+            Command.QUERY,
+            attribute_0=0,
+            attribute_1=2,
+            body_length=len(root),
+            body=root
+        )
+        self.__request(request)
